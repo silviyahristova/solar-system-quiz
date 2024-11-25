b07939d59b89ac63b07dc2bb5610db992df8316f
Silviya Hristova <Bliznetatata@gmail.com>
2024-11-25 18:47:38 +0000

    feat: add function for showing the questions and answers , and function for counting the questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,16 @@ const exitButton = document.getElementById('exit-btn');
 const main = document.getElementById('main');
 const continueButton = document.getElementById('continue-btn');
 const quizBox = document.getElementById('quiz-box');
+const nextButton = document.getElementById('next-button');
+const restartButton = document.getElementById('restart-button');
+const answerList = document.querySelector(`.answers-list`)
 
+
+
+let questionCount = 0;
+let maxQuestions = 10;
+let questionNumb = 1;
+let username;
 
 startButton.onclick = () => {
     rulesBox.classList.add('active');
@@ -20,4 +29,44 @@ continueButton.onclick = () => {
     quizBox.classList.add('active');
     rulesBox.classList.remove('active');
     main.classList.remove('active');
+    showQuestion(0);
+    questionCounter(1);
+}
+
+nextButton.onclick = () => {
+    if(questionCount < myQuestions.length -1){
+    questionCount++;
+    showQuestion(questionCount);
+
+    questionNumb++;
+    questionCounter(questionNumb);
+
+    }else{
+        console.log('Questions Completed!')
+    }
+}
+
+restartButton.onclick = () => {
+    quizBox.classList.add('active');
+    rulesBox.classList.remove('active');
+    main.classList.remove('active');
+}
+
+//question and option function
+
+function showQuestion(index) {
+    const questionText = document.getElementById('question-text');
+    questionText.textContent = `${myQuestions[index].questionNumber}. ${myQuestions[index].questionText}`;
+    
+    let optionTag = `<div class="answer-button"><span>${myQuestions[index].options[0]}</span></div>
+    <div class="answer-button"><span>${myQuestions[index].options[1]}</span></div>
+    <div class="answer-button"><span>${myQuestions[index].options[2]}</span></div>
+    <div class="answer-button"><span>${myQuestions[index].options[3]}</span></div>`;
+
+    answerList.innerHTML  = optionTag;
+}
+
+function questionCounter (index) {
+    const questionTotal = document.querySelector('.count-question');
+    questionTotal.textContent = `Question ${index} of ${maxQuestions}`;
 }
