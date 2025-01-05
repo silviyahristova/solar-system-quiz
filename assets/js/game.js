d0d9cc6c2751bf09331130fd3b35cda89f35fda9
Silviya Hristova <Bliznetatata@gmail.com>
2025-01-05 14:43:26 +0000

    feat: move function for logo and footer to a new js file to be able to add to all html without having all the not needed function in the rest of the html files

--- assets/js/game.js
+++ assets/js/game.js
@@ -45,8 +45,7 @@ startButton.onclick = () => {
     } else {
         // If the username is valid
         rulesBox.classList.add('active'); // Show the rules box
-        main.classList.add('active'); 
-        console.log(`Welcome, ${enteredName}!`); // Log the username
+        main.classList.remove('active'); 
     }
 };
 
@@ -134,67 +133,6 @@ startAgainButton.onclick = () => {
 
     quizOn = true;
 }
-
-//Keep logo in all main divs
-const headerHTML = `
-  <header id="logo">
-    <div class="row">
-      <div class="col-6">
-        <a class="logo" href="index.html">
-          <img
-            src="assets/images/homepage/logo.webp"
-            alt="The planet Earth"
-            id="logoimage"
-            srcset="assets/images/homepage/logo.webp" />
-        </a>
-      </div>
-    </div>
-  </header>
-`;
-
-const divs = document.querySelectorAll('.box-logo');
-
-divs.forEach(div => {
-  const tempDiv = document.createElement('div');
-  tempDiv.innerHTML = headerHTML;
-  const headerElement = tempDiv.firstElementChild; 
-
-  div.prepend(headerElement);
-});
-
-// Keep footer in all main divs
-const footerHTML = `
-  <footer class="container-fluid p-0" id="footer">
-    <div class="logo-link">
-      <a
-        class="social-media-link"
-        href="https://github.com/silviyahristova"
-        aria-label="GitHub"
-        target="_blank"
-        ><i class="fa-brands fa-github"></i>
-      </a>
-      <a
-        class="social-media-link"
-        href="https://www.linkedin.com/in/silviyahristova93/"
-        aria-label="LinkedIn"
-        target="_blank"
-        ><i class="fa-brands fa-linkedin-in"></i>
-      </a>
-    </div>
-    <div class="text-centre">
-      <span>Made by Silviya Hristova</span>
-    </div>
-  </footer>
-`;
-
-const footerDiv = document.querySelectorAll('.boxes-logo');
-
-divs.forEach(div => {
-  const tempDiv = document.createElement('div');
-  tempDiv.innerHTML = footerHTML; 
-  const footerElement = tempDiv.firstElementChild; 
-  div.appendChild(footerElement);
-});
 
 // Question and option function
 // Shuffle the questions array
